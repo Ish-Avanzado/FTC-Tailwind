f2042a4d64c4ddcbc885aa944797e408f747b7f4
Ish-Avanzado <isiaavanzado@gmail.com>
2025-05-07 16:09:01 +0800

    updating files

--- dist/script.js
+++ dist/script.js
@@ -4,3 +4,33 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("open");
   hNav.classList.toggle("open");
 });
+
+// faq
+
+// const faqs = document.querySelectorAll(".faq__nav");
+// faqs.forEach((faq) => {
+//   faqs.addEventListener("click", () => {
+//     faqs.querySelector(".faq__pop").classList.toggle("open");
+//     faqs.nextElementSibling.classList.toggle("open");
+//   });
+// });
+
+
+const faqNavs = document.querySelectorAll(".faq__nav");
+
+faqNavs.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    const FaqItem = nav.parentElement;
+    const FaqPop = FaqItem.querySelector(".faq__pop");
+
+    document.querySelectorAll(".faq__pop").forEach((pop) => {
+      if (pop !== FaqPop) {
+        pop.classList.remove("open");
+        pop.previousElementSibling.classList.remove("active");
+      }
+    });
+
+    FaqPop.classList.toggle("open");
+    nav.classList.toggle("active");
+  });
+});
